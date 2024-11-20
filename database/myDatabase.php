0afeac56b176f4ed1181874005617e681fa44826
Sean Ratemo <sean.ratemo@gmail.com>
2024-11-20 14:36:58 +0300

    MySQL server

--- database/myDatabase.php
+++ database/myDatabase.php
@@ -4,10 +4,11 @@ $servername = "nexus-azure-server.mysql.database.azure.com"; // Azure MySQL serv
 $username = "rat3mo"; // Azure MySQL username with server name
 $password = "Marindi@12"; // Your Azure MySQL password
 $dbname = "gaming"; // Your database name
+$ssl_ca = "/Users/seanratemo/Downloads/DigiCertGlobalRootCA.crt.pem";
 
 // create the database connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); // Enable SSL/TLS connection
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL); // Enable SSL/TLS connection
 mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
 
 // verify the db connection
