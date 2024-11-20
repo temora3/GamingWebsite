9a1ced59afb60e6611ddae71a0f3330b656e2714
Sean Ratemo <sean.ratemo@gmail.com>
2024-11-21 01:24:33 +0300

    Connection SQL

--- database/myDatabase.php
+++ database/myDatabase.php
@@ -6,17 +6,13 @@ $password = 'Marindi@12';
 $dbname = 'gaming';
 $port = 3306; // Default MySQL port
 
-// SSL Configuration (recommended for Azure)
-$ssl_ca = '/Users/seanratemo/Downloads/DigiCertGlobalRootCA.crt.pem'; // Download this from Azure portal
-
 try {
-    // Create PDO connection with SSL options
+    // Create PDO connection without SSL
     $dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4";
     
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca, // SSL certificate
         PDO::ATTR_EMULATE_PREPARES => false,
     ];
 
